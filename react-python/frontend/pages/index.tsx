333172b14548e88c03730c534890f12b90320ffd
Alexssandro Bianchi <alexssandro.bianchi@intelltech.com>
2022-10-21 20:27:03 -0300

    Iniciado projeto com react

---
+++ react-python/frontend/pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+import Cabecalho from "../src/components/Cabecalho/Cabecalho";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Cabecalho />
+    </div>
+  );
+};
+
+export default Home;
---
+++ react-python/frontend/src/components/Cabecalho/Cabecalho.tsx
@@ -0,0 +1,11 @@
+import { CabecalhoContainer, Logo } from "./Cabecalho.style";
+
+const Cabecalho = () => {
+  return <CabecalhoContainer>
+      <div>
+          <Logo src="/images/myteacher.png" />
+      </div>
+  </CabecalhoContainer>;
+};
+
+export default Cabecalho;
