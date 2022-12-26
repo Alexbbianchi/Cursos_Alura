7d07604b77f4eb9021b4e9b32a6de5e93349d1aa
Alex Bianchi <alexbbianchi@gmail.com>
2022-12-25 21:18:53 -0300

    Criado classes e primeira rota

--- react-python/backend/myteacher/urls.py
+++ react-python/backend/myteacher/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from teacher.views import TeacherAPIView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('teachers/', TeacherAPIView.as_view())
 ]
--- react-python/backend/teacher/views.py
+++ react-python/backend/teacher/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.views import Response
 
-# Create your views here.
+from teacher.models import Teacher
+from teacher.serializers import TeacherSerializer
+
+class TeacherAPIView(APIView):
+    def get(self, request, format=None):
+        teachers = Teacher.objects.all()
+        serializer = TeacherSerializer(teachers, many=True)
+        return Response(serializer.data, status=200)
