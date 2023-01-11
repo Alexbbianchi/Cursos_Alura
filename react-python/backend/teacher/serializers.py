dbf6f6733764d6a89bccec1e2615b212ecd5f918
Alex Bianchi <alexbbianchi@gmail.com>
2023-01-10 21:27:08 -0300

    Removido projeto do python

--- react-python/backend/teacher/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from teacher.models import Teacher
-
-class TeacherSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Teacher
-        # fields = ('id', 'name', 'hour_value', 'description', 'photo')
-        fields = '__all__'
